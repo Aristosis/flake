709dd0b12e1a6d1ae74ae25a6098125a99b188b4
Ari <communeofstars@proton.me>
2026-06-25 20:44:15 +0530

    Fix: add networking.nix to modules/nixos/default.nix

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -5,5 +5,6 @@
   desktop = import ./desktop.nix;
   displayManager = import ./display-manager.nix;
   graphicsDrivers = import ./graphics-drivers.nix;
+  networking = import ./networking.nix;
   sshRemote = import ./remote-ssh.nix;
 }
