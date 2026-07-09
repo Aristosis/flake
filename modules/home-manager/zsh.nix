27c38ec1abe12946d5056354b759d391a4e0800b
Ari <communeofstars@proton.me>
2026-07-09 17:22:38 +0530

    rename and revise aliases

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -31,9 +31,8 @@
         cat = "bat";
         btw = "fastfetch";
         dlp = ''yt-dlp --extract-audio --audio-format best --embed-metadata --embed-thumbnail --audio-quality 0 -o "%(artist)s - %(track)s.%(ext)s"'';
-        nrs = "sudo nixos-rebuild switch --flake $HOME/nix-dotfiles/";
-        dots = "git --git-dir=$HOME/dotfiles/.git --work-tree=$HOME/dotfiles";
-        lazydots = "lazygit --git-dir=$HOME/dotfiles/.git --work-tree=$HOME/dotfiles";
+        nrs = "sudo nixos-rebuild switch --flake $HOME/flake";
+        hrs = "home-manager switch --flake $HOME/flake -b backup";
       };
 
       # Use cached compinit. Only rebuild if system generation newer than current cache
@@ -58,9 +57,6 @@
         setopt glob_complete
         setopt pushd_ignore_dups
         setopt prompt_subst
-
-        source <(zoxide init zsh --cmd cd)
-        source <(fzf --zsh)
 
         zstyle ':completion:*' group-name \'\'
         zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
