e7afb17b8b1ff55978bb1951d54ad3ef858ccd2c
Ari <communeofstars@proton.me>
2026-07-11 13:40:32 +0530

    test package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,11 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
 pkgs: {
+  nixos-btw = pkgs.writeShellScriptBin "nixos-btw" ''
+    printf "test package\n"
+    printf "I use\n"
+    fastfetch
+    printf "btw\n"
+  '';
   # example = pkgs.callPackage ./example { };
 }
