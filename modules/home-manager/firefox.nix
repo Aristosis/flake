e592b0a3e3478a431c90b2893b451fc8a626761c
Ari <communeofstars@proton.me>
2026-07-11 11:45:34 +0530

    firefox home manager config

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -6,31 +6,50 @@
 }: {
   options.features.home-manager.firefox.enable = lib.mkEnableOption "Enable firefox";
 
+  config.stylix.targets.firefox.profileNames = [ "default" ];
   config.programs.firefox = lib.mkIf config.features.home-manager.firefox.enable {
     enable = true;
 
     languagePacks = ["en-IN"];
 
+    profiles.default = {
+      settings = {
+        "browser.urlbar.suggest.searches" = false;
+        "browser.toolbars.bookmarks.visibility" = "never";
+        "sidebar.verticalTabs" = true;
+      };
+    };
+
     policies = {
       AppAutoUpdate = false;
+      NewTabPage = false;
       BackgroundAppUpdate = false;
-
-      DisableFirefoxStudies = true;
-      DisableFirefoxAccounts = true;
-      DisableFirefoxScreenshots = true;
-      DisableForgetButton = true;
-      DisableMasterPasswordCreation = true;
-      DisableProfileImport = true;
-      DisableProfileRefresh = true;
-      DisableSetDesktopBackground = true;
+      DisplayMenuBar = "never";
       DisablePocket = true;
       DisableTelemetry = true;
       DisableFormHistory = true;
       DisablePasswordReveal = true;
-
-      DisplayMenuBar = "never";
-      HardwareAcceleration = true;
+      DisableSetDesktopBackground = true;
+      DisableProfileImport = true;
+      AutofillAddressEnabled = false;
       OfferToSaveLogins = false;
+      HardwareAcceleration = true;
+      SkipTermsOfUse = true;
+      GenerativeAI.Enabled = false;
+      FirefoxHome = {
+        SponsoredTopSites = false;
+        Highlights = false;
+        SponsoredHighlights = false;
+        SponsoredStories = false;
+      };
+      UserMessaging = {
+        FeatureRecommendations = false;
+        ExtensionRecommendations = false;
+        UrlBarInterventions = false;
+        FirefoxLabs = false;
+        MoreFromMozilla = false;
+        SkipOnboarding = true;
+      };
     };
   };
 }
