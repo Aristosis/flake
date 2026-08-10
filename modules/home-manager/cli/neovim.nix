1b708e2539bf1c684aebb5d129f372b8ab89672d
Ari <communeofstars@proton.me>
2026-08-10 16:21:51 +0530

    fix massive jujutsu fuckups

--- modules/home-manager/cli/neovim.nix
+++ modules/home-manager/cli/neovim.nix
@@ -20,26 +20,3 @@
       config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/flake/modules/home-manager/config/nvim";
   };
 }
-{
-  lib,
-  config,
-  pkgs,
-  ...
-}:
-{
-
-  options.features.home-manager.cli.neovim.enable = lib.mkOption {
-    description = "Enable Neovim configuration";
-    default = config.features.home-manager.cli.enable;
-  };
-
-  config = lib.mkIf config.features.home-manager.cli.neovim.enable {
-    home.packages = with pkgs; [
-      tree-sitter
-      neovim
-    ];
-
-    xdg.configFile.nvim.source =
-      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/flake/modules/home-manager/config/nvim";
-  };
-}
--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,8 +1,6 @@
 {
+  cli = import ./cli;
   firefox = import ./firefox.nix;
   niri = import ./niri;
-  cli = import ./cli;
   umu = import ./umu.nix;
-  cli = import ./cli;
-  gaming = import ./gaming.nix;
 }
