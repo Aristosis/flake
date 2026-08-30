4286d2da0007023d67daa776cd50ef7d998b8723
Ari <communeofstars@proton.me>
2026-08-30 11:06:30 +0530

    add new drive

--- hosts/nixos-desktop/configuration.nix
+++ hosts/nixos-desktop/configuration.nix
@@ -19,7 +19,7 @@
       (modules + "desktop.nix")
       (modules + "nvidia-drivers.nix")
       (modules + "remote-ssh.nix")
-      (modules + "theme.nix")
+      (modules + "themes.nix")
       (modules + "zram.nix")
 
       (modules + "programs")
