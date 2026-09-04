29b9c209b8ef197303e9005e824e1eb1230ca2ce
Ari <communeofstars@proton.me>
2026-09-04 19:50:07 +0530

    add test game desktop entry

--- modules/pkgs/gamering.nix
+++ modules/pkgs/gamering.nix
@@ -6,8 +6,7 @@ export __GLX_VENDOR_LIBRARY_NAME=nvidia
 export __VK_LAYER_NV_optimus=NVIDIA_only
 
 WINE=$HOME/Media/Share/Games/Wine
-
 export WINEPREFIX="$WINE/Wineprefix"
 
-umu-run $@
+umu-run "$@"
 ''
--- modules/programs/games/umu-launcher.nix
+++ modules/programs/games/umu-launcher.nix
@@ -3,10 +3,31 @@
   pkgs,
   ...
 }: {
-  hjem.users.ari.packages = with pkgs; [
-    umu-launcher
-    (
-      import "${inputs.self}/modules/pkgs/gamering.nix" pkgs
-    )
-  ];
+  hjem.users.ari = {
+    packages = with pkgs; [
+      umu-launcher
+      (
+        import "${inputs.self}/modules/pkgs/gamering.nix" pkgs
+      )
+    ];
+    files = 
+    let
+      entries = ".local/share/applications";
+      in
+      {
+      "${entries}/ULTRAKILL.desktop".text =
+        ''
+[Desktop Entry]
+Encoding=UTF-8
+Value=1.0
+Type=Application
+Name=ULTRAKILL
+Categories=Game;
+
+Icon=/home/ari/Share/Games/ULTRAKILL/icon.png
+Path=/home/ari/Share/Games/ULTRAKILL
+Exec="gamering" "/home/ari/Share/Games/ULTRAKILL/ULTRAKILL.exe"
+        '';
+    };
+  };
 }
