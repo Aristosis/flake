94bab29e2af456174309218402fe4d08bcb5bda4
Ari <communeofstars@proton.me>
2026-09-05 12:29:03 +0530

    nix fmt

--- modules/pkgs/gamering.nix
+++ modules/pkgs/gamering.nix
@@ -1,12 +1,12 @@
 pkgs:
 pkgs.writeShellScriptBin "gamering" ''
-export __NV_PRIME_RENDER_OFFLOAD=1
-export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-export __GLX_VENDOR_LIBRARY_NAME=nvidia
-export __VK_LAYER_NV_optimus=NVIDIA_only
+  export __NV_PRIME_RENDER_OFFLOAD=1
+  export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+  export __GLX_VENDOR_LIBRARY_NAME=nvidia
+  export __VK_LAYER_NV_optimus=NVIDIA_only
 
-WINE=$HOME/Media/Share/Games/Wine
-export WINEPREFIX="$WINE/Wineprefix"
+  WINE=$HOME/Media/Share/Games/Wine
+  export WINEPREFIX="$WINE/Wineprefix"
 
-umu-run "$@"
+  umu-run "$@"
 ''
--- modules/programs/games/umu-launcher.nix
+++ modules/programs/games/umu-launcher.nix
@@ -2,47 +2,44 @@
   inputs,
   pkgs,
   ...
-}: {
+}:
+{
   hjem.users.ari = {
     packages = with pkgs; [
       umu-launcher
-      (
-        import "${inputs.self}/modules/pkgs/gamering.nix" pkgs
-      )
+      (import "${inputs.self}/modules/pkgs/gamering.nix" pkgs)
     ];
-    files = 
-    let
-      entries = ".local/share/applications";
+    files =
+      let
+        entries = ".local/share/applications";
       in
 
       {
-      "${entries}/ULTRAKILL.desktop".text =
-        ''
-[Desktop Entry]
-Encoding=UTF-8
-Value=1.0
-Type=Application
-Name=ULTRAKILL
-Categories=Game;
+        "${entries}/ULTRAKILL.desktop".text = ''
+          [Desktop Entry]
+          Encoding=UTF-8
+          Value=1.0
+          Type=Application
+          Name=ULTRAKILL
+          Categories=Game;
 
-Icon=/home/ari/Share/Games/ULTRAKILL/icon.png
-Path=/home/ari/Share/Games/ULTRAKILL
-Exec="gamering" "/home/ari/Share/Games/ULTRAKILL/ULTRAKILL.exe"
+          Icon=/home/ari/Share/Games/ULTRAKILL/icon.png
+          Path=/home/ari/Share/Games/ULTRAKILL
+          Exec="gamering" "/home/ari/Share/Games/ULTRAKILL/ULTRAKILL.exe"
         '';
 
-      "${entries}/GTA III.desktop".text =
-          ''
-[Desktop Entry]
-Encoding=UTF-8
-Value=1.0
-Type=Application
-Name=GTA III
-Categories=Game;
+        "${entries}/GTA III.desktop".text = ''
+          [Desktop Entry]
+          Encoding=UTF-8
+          Value=1.0
+          Type=Application
+          Name=GTA III
+          Categories=Game;
 
-Icon=/home/ari/Share/Games/GTA III/Icons/icon.png
-Path=/home/ari/Share/Games/GTA III
-Exec="WINEDLLOVERRIDES="d3d8=n,b"" "gamering" "/home/ari/Share/Games/GTA III/gta3.exe"
-          '';
-    };
+          Icon=/home/ari/Share/Games/GTA III/Icons/icon.png
+          Path=/home/ari/Share/Games/GTA III
+          Exec="WINEDLLOVERRIDES="d3d8=n,b"" "gamering" "/home/ari/Share/Games/GTA III/gta3.exe"
+        '';
+      };
   };
 }
