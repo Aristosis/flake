e524202a4e0737e60c9470313447bb07d4a2ed3b
Ari <communeofstars@proton.me>
2026-09-06 13:22:03 +0530

    functional enough

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,17 +1,23 @@
 vim.pack.add {
    { src = "https://github.com/nvim-tree/nvim-web-devicons" },
 
-   { src = "https://github.com/Saghen/blink.cmp", version = vim.version.range("1.*") },
    { src = "https://github.com/stevearc/oil.nvim" },
+   { src = "https://github.com/stevearc/conform.nvim" },
    { src = "https://github.com/ibhagwan/fzf-lua" },
    { src = "https://github.com/kylechui/nvim-surround" },
-
-   { src = "https://github.com/lewis6991/gitsigns.nvim" }
+   { src = "https://github.com/neovim/nvim-lspconfig" },
+   { src = "https://github.com/lewis6991/gitsigns.nvim" },
 }
 
-require("blink.cmp").setup()
-require("gitsigns").setup()
 require("nvim-surround").setup()
+require("gitsigns").setup()
+
+require("conform").setup {
+   format_on_save = true,
+   formatters_by_ft = {
+      lua = { "stylua" },
+   },
+}
 
 local oil = require("oil")
 oil.setup {
@@ -30,31 +36,13 @@ end)
 
 local fzf = require("fzf-lua")
 fzf.setup {
-   fzf_opts = { ["--color"] = "bg:-1,bg+:-1,gutter:-1" }
+   fzf_opts = { ["--color"] = "bg:-1,bg+:-1,gutter:-1" },
 }
 
-vim.keymap.set(
-   "n",
-   "<leader><leader>",
-   function()
-      if not fzf.git_files() then fzf.files() end
-   end
-)
+vim.keymap.set("n", "<leader><leader>", function()
+   if not fzf.git_files() then fzf.files() end
+end)
 
-vim.keymap.set(
-   "n",
-   "<leader>m",
-   fzf.marks
-)
+vim.keymap.set("n", "<leader>m", fzf.marks)
 
-vim.keymap.set(
-   "n",
-   "<leader>f",
-   fzf.live_grep
-)
-
-vim.keymap.set(
-   "n",
-   "<leader>f",
-   fzf.live_grep
-)
+vim.keymap.set("n", "<leader>f", fzf.live_grep)
