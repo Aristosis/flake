ca0b04158808b6ccbc43c9afdd2bcd84660567cf
Ari <communeofstars@proton.me>
2026-09-14 11:25:20 +0530

    mini

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -3,14 +3,19 @@ vim.pack.add {
 
    { src = "https://github.com/stevearc/oil.nvim" },
    { src = "https://github.com/stevearc/conform.nvim" },
+   { src = "https://github.com/nvim-mini/mini.nvim" },
    { src = "https://github.com/ibhagwan/fzf-lua" },
-   { src = "https://github.com/kylechui/nvim-surround" },
    { src = "https://github.com/neovim/nvim-lspconfig" },
    { src = "https://github.com/lewis6991/gitsigns.nvim" },
    { src = "https://github.com/obsidian-nvim/obsidian.nvim", version = vim.version.range("*") },
 }
 
-require("nvim-surround").setup()
+require("mini.move").setup()
+require("mini.surround").setup()
+require("mini.statusline").setup()
+require("mini.ai").setup()
+require("mini.splitjoin").setup()
+
 require("gitsigns").setup()
 
 require("conform").setup {
@@ -44,7 +49,7 @@ fzf.setup {
 vim.keymap.set("n", "<leader><leader>", fzf.files)
 vim.keymap.set("n", "<leader>f", fzf.live_grep)
 vim.keymap.set("n", "<leader>b", fzf.buffers)
-vim.keymap.set("n", "<leader>f", fzf.files)
+vim.keymap.set("n", "<leader>f", fzf.live_grep)
 vim.keymap.set("n", "<leader>m", fzf.marks)
 
 vim.schedule(function()
