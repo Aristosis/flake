b604afec7c24cb1917a4e45ac43e634ad0bb0003
ari <communeofstars@proton.me>
2026-09-15 13:40:58 +0530

    remove ly-session.log from home

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -4,7 +4,10 @@
   ];
 
   services = {
-    displayManager.ly.enable = true;
+    displayManager.ly = {
+      enable = true;
+      settings.session_log = ".local/state/ly-session.log";
+    };
     gvfs.enable = true;
   };
 
