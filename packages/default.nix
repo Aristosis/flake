5b4fb077c50ef23439da443eee03464a732e0a98
ari <communeofstars@proton.me>
2026-09-15 11:45:55 +0530

    wrap fuzzel

--- packages/default.nix
+++ packages/default.nix
@@ -5,5 +5,6 @@
   fzf-wrapped = pkgs.callPackage ./fzf.nix { };
   kitty-wrapped = pkgs.callPackage ./kitty { };
   bat-wrapped = pkgs.callPackage ./bat.nix { };
+  fuzzel-wrapped = pkgs.callPackage ./fuzzel { };
   gamering = pkgs.callPackage ./umu-launcher-wrapped.nix { };
 }
---
+++ packages/fuzzel/default.nix
@@ -0,0 +1,12 @@
+{ fuzzel, makeWrapper, symlinkJoin }:
+symlinkJoin {
+  name = "fuzzel-wrapped";
+  paths = [ fuzzel ];
+  nativeBuildInputs = [ makeWrapper ];
+  postBuild =
+  ''
+    wrapProgram $out/bin/fuzzel \
+      --add-flags "--config=${./fuzzel.ini}"
+  '';
+  meta.mainProgram = "fuzzel";
+}
